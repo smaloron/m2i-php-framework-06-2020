0107d46bf784ba12e85ae0ab590b5bdb2fd9335d
Sebastien Maloron <contact@smaloron.eu>
2020-09-16 15:43:26 +0200

    Formulaire d'inscription

--- controllers/inscription.php
+++ controllers/inscription.php
@@ -1,5 +1,45 @@
 <?php
 
+// Récupération des données
+$rules = [
+    "user_name" => FILTER_SANITIZE_STRING,
+    "user_email" => FILTER_VALIDATE_EMAIL,
+    "user_email_confirm" => FILTER_VALIDATE_EMAIL,
+    "user_password" => FILTER_DEFAULT,
+    "user_password_confirm" => FILTER_DEFAULT
+];
+
+$userData = filter_input_array(INPUT_POST, $rules);
+
+// Validation des données
+$isPosted = count($_POST) > 0;
+$errors = [];
+
+if ($isPosted) {
+    if (empty($userData["user_name"])) {
+        array_push($errors, "Le nom ne peut être vide");
+    }
+    if (empty($userData["user_email"])) {
+        array_push($errors, "L'adresse email' ne peut être vide");
+    }
+    if (empty($userData["user_password"])) {
+        array_push($errors, "Le mot de passe ne peut être vide");
+    }
+
+    if (mb_strlen($userData["user_password"]) < 4) {
+        array_push($errors, "Le mot de passe doit comporter au moins 4 caractères");
+    }
+
+    if ($userData["user_password"] != $userData["user_password_confirm"]) {
+        array_push($errors, "Le mot de passe et la confirmation doivent être identiques");
+    }
+
+    if ($userData["user_email"] != $userData["user_email_confirm"]) {
+        array_push($errors, "L'adresse email et la confirmation doivent être identiques");
+    }
+}
+
+// Affichage du formulaire
 $pageTitle = "Inscription des utilisateurs";
 $page = "view-inscription";
 
--- views/view-inscription.php
+++ views/view-inscription.php
@@ -1,1 +1,32 @@
-<h1>Inscription des utilisateurs</h1>
+<div class="col-md-8 col-lg-6">
+    <h1>Inscription des utilisateurs</h1>
+
+    <!-- Affichage des erreurs -->
+
+    <form method="post">
+        <div class="form-group">
+            <label>Nom</label>
+            <input type="text" name="user_name" class="form-control">
+        </div>
+        <div class="form-group">
+            <label>Email</label>
+            <input type="email" name="user_email" class="form-control">
+        </div>
+        <div class="form-group">
+            <label>Confirmation de l'email</label>
+            <input type="email" name="user_email_confirm" class="form-control">
+        </div>
+        <div class="form-group">
+            <label>Mot de passe</label>
+            <input type="password" name="user_password" class="form-control">
+        </div>
+        <div class="form-group">
+            <label>Confirmation du mot de passe</label>
+            <input type="password" name="user_password_confirm" class="form-control">
+        </div>
+
+        <button type="submit" class="btn btn-success btn-block">
+            Valider
+        </button>
+    </form>
+</div>
