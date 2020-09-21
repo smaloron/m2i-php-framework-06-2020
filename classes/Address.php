a3de57fb15080c078ee11c4f4b20c1069ccfa3fe
Sebastien Maloron <contact@smaloron.eu>
2020-09-21 16:53:13 +0200

    classe adresse

---
+++ classes/Address.php
@@ -0,0 +1,48 @@
+<?php
+
+class Address
+{
+
+    private $street;
+
+    private $zipCode;
+
+    private $city;
+
+    public function __construct($street, $zip, $city)
+    {
+        $this->street = $street;
+        $this->zipCode = $zip;
+        $this->city = $city;
+    }
+
+    function getStreet()
+    {
+        return $this->street;
+    }
+
+    function setStreet($value)
+    {
+        $this->street = $value;
+    }
+
+    function getzipCode()
+    {
+        return $this->zipCode;
+    }
+
+    function setZipCode($value)
+    {
+        $this->zipCode = $value;
+    }
+
+    function getCity()
+    {
+        return $this->city;
+    }
+
+    function setCity($value)
+    {
+        $this->city = $value;
+    }
+}
